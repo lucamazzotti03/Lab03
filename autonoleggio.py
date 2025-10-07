c0cce8f995d20430fec3fd5f48f96a89438043d5
lucamazzotti03 <luchinomazzotti01@gmail.com>
2025-10-07 14:18:33 +0200

    lab03.prova

--- autonoleggio.py
+++ autonoleggio.py
@@ -1,13 +1,39 @@
+class Automobile:
+    def __init__(self, codice, marca, modello, anno, nPosti):
+        self.codice = codice
+        self.marca = marca
+        self.modello = modello
+        self.anno = int(anno)
+        self.nPosti = int(nPosti)
+
 class Autonoleggio:
     def __init__(self, nome, responsabile):
-        """Inizializza gl i attributi e le strutture dati"""
+        self.nome = nome
+        self.responsabile = responsabile
+        self.Automobile = []
+
+
+        """Inizializza gli attributi e le strutture dati"""
         # TODO
 
+
     def carica_file_automobili(self, file_path):
-        """Carica le auto dal  file"""
+        try:
+            file = open(file_path, 'r')
+        except FileNotFoundError:
+            print(f"file {file_path} non trovato")
+        for riga in file:
+            riga = riga.rstrip().split(",")
+            auto = Automobile(riga[0], riga[1], riga[2], riga[3], riga[4])
+            print(auto.codice, auto.marca, auto.modello, auto.anno, auto.nPosti)
+            self.Automobile.append(auto)
+
+        """Carica le auto dal file"""
         # TODO
 
     def aggiungi_automobile(self, marca, modello, anno, num_posti):
+        auto = Automobile(codice, marca, modello, anno, num_posti)
+        self.Automobile.append(auto)
         """Aggiunge un'automobile nell'autonoleggio: aggiunge solo nel sistema e non aggiorna il file"""
         # TODO
 
